dbfca82bf5ba108d80ce4305e8aa7bf42813a56f
raeq <richard@imac.local>
2018-05-02 23:23:58 +0200

    Added pipfile, improved logging in certs.py

--- awschecker/certs.py
+++ awschecker/certs.py
@@ -7,7 +7,6 @@ from .classes import AWSCertificate
 from .classes import AWSARN
 
 
-
 def find_certs():
 
     logger = logging.getLogger(__name__)
@@ -17,12 +16,12 @@ def find_certs():
     acm_regions = s.get_available_regions('acm')
 
     for region in acm_regions:
-        logger.debug("Searching region: %s",region)
+        logger.debug("Searching region: %s", region)
         acmclient = boto3.client('acm', region_name=region)
         response = acmclient.list_certificates()
 
         for cert in response.get("CertificateSummaryList"):
-            logger.debug("The cert header: %s",cert)
+            logger.debug("The cert header: %s", cert)
 
             description = acmclient.describe_certificate(
                 CertificateArn=cert['CertificateArn'])
@@ -34,17 +33,19 @@ def find_certs():
                                     description['Certificate']['Serial']
                                     )
             logger.info(mycert)
-            all_certs[mycert.ARN]=mycert
+            all_certs[mycert.ARN] = mycert
 
     logger.debug(all_certs)
     return all_certs
 
+
 def check_certs():
     logger = logging.getLogger(__name__)
-    all_arns=find_certs()
+    all_arns = find_certs()
     for arn in all_arns:
         mycert = all_arns[arn]
         if mycert.Status != "ISSUED":
             logger.warn("Cert %s is not valid.", arn)
         if mycert.CertificateTransparencyLoggingPreference != "Enabled":
-            logger.warn("On cert %s certificate transparency logging is enabled.", arn)
+            logger.warn(
+                "On cert %s certificate transparency logging is enabled.", arn)
