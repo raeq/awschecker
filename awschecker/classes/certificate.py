38a06c76737f17cbc23987cf5047e6bff7e05ea2
subzero-997 <quinn.richard@gmail.com>
2018-05-12 01:40:56 +0200

    Pylinter found a bunch of crap.

--- awschecker/classes/certificate.py
+++ awschecker/classes/certificate.py
@@ -1,40 +1,8 @@
 
 import logging
-from .arn import AWSARN
 from .awsobject import AWSObject
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, TIMESTAMP
-import datetime
-from boto3.session import Session
-import boto3
-from ..decorator_logging import logged
-import constants
-
-Base = declarative_base()
 
 # arn #timestamp #region #account #boto3client #metdata
-
-@logged
-class CertTable(Base):
-    logger = logging.getLogger(__name__)
-    __tablename__ = 'certificates'
-
-    ARN = Column(String, primary_key=True)
-    DomainName = Column(String)
-    region = Column(String)
-    account = Column(String)
-    Status = Column(String)
-    Serial = Column(String)
-    timestamp = Column(TIMESTAMP)
-
-    def __repr__(self):
-        return "<Certificate(ARN='%s', \
-        DomainName='%s', region='%s', account='%s',  \
-        Status='%s', region='%s', Serial='%s')>" % (
-            self.ARN, self.DomainName,
-            self.region, self.account, self.password,
-            self.Status, self.Serial)
-
 
 class AWSCertificate(AWSObject):
     """Represents an AWS ACM certificate object."""
@@ -47,7 +15,7 @@ class AWSCertificate(AWSObject):
                 An AWSCertificate object instantiated from the raw JSON.
         """
         self.logger = logging.getLogger(__name__)
-        super(__class__,self).__init__(description['CertificateArn'])
+        super(__class__, self).__init__(description['CertificateArn'])
 
         self.DomainName = description['DomainName']
         self.Status = description['Status']
@@ -71,36 +39,12 @@ class AWSCertificate(AWSObject):
         """Classmethod to disable certificate transparency logging on this certificate"""
 
         if self.Issuer == 'Amazon' and self.Type == 'AMAZON_ISSUED':
-
-            s = Session()
-            acmclient = boto3.client('acm', region_name=self.ARN.region)
-
-            response = acmclient.update_certificate_options(
-                CertificateArn=self.ARN.arn,
-                Options={
-                    'CertificateTransparencyLoggingPreference': 'DISABLED'
-                }
-            )
-
-            #response = acmclient.update_certificate_options(CertificateArn=self.ARN.arn, Options=mOptions)
             self.logger.warn(
                 "Cert %s had cert tran logging disable successfully", self.url)
 
     def enable_transparency_logging(self):
         """Classmethod to enable certificate transparency logging on this certificate"""
         if self.Issuer == 'Amazon' and self.Type == 'AMAZON_ISSUED':
-
-            s = Session()
-            acmclient = boto3.client('acm', region_name=self.ARN.region)
-
-            response = acmclient.update_certificate_options(
-                CertificateArn=self.ARN.arn,
-                Options={
-                    'CertificateTransparencyLoggingPreference': 'ENABLED'
-                }
-            )
-
-            #response = acmclient.update_certificate_options(CertificateArn=self.ARN.arn, Options=mOptions)
             self.logger.warn(
                 "Cert %s had cert tran logging enabled successfully", self.url)
 
